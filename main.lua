8e4ad6ec0045b0be388f40a5d8c6fdc688eb3b4f
Levandro <mogilaaa@list.ru>
2015-07-14 14:13:10 +0300

    Square

--- main.lua
+++ main.lua
@@ -1,56 +1,30 @@
 function love.load()
-love.graphics.setBackgroundColor(255,255,255)
-xCloud = 0
-imageCloud = love.graphics.newImage( "textures/cloud.png")
-imageEnemy_1 = love.graphics.newImage( "textures/enemy1.png")
-imageEnemy_2 = love.graphics.newImage( "textures/enemy2.png")
-imageGround = love.graphics.newImage( "textures/ground.png")
+  love.graphics.setBackgroundColor(255,255,255)
+  box = {}
+  box.x = 0
+  box.y = 250
+  box.w = 50
+  box.h = 50
+  box.speed =200
+  box.direction = 1
 end
 
 function love.draw()
-local x = love.mouse.getX( )
-local y = love.mouse.getY( )
-
-love.graphics.setColor( 198, 241, 255, 255 )
-love.graphics.rectangle( "fill",0,0,800,500 )
-
-love.graphics.setColor( 255, 255, 255, 255 )
-
-love.graphics.draw(imageCloud,xCloud - 256,128,0,1,1,0,0)
-
-love.graphics.setColor( 103, 164, 21, 255 )
-love.graphics.rectangle( "fill",0,400,800,300 )
-
-love.graphics.setColor( 255, 255, 255, 255 )
-love.graphics.draw(imageGround,(800-1024)/2,400-64,0,1,1,0,0)
-
+  love.graphics.setColor(255,1,1)
+  love.graphics.rectangle("fill",box.x,box.y,box.w,box.h)
 end
 
+    
+ 
 function love.update(dt)
-xCloud =xCloud + 32*dt
-if xCloud >= (800+128) then 
-  xCloud = 0
- end 
+  box.x = box.x + dt*box.speed*box.direction
+if box.x >= 600 then
+box.direction = -1
+box.x = 600 - dt*box.speed*box.direction
 end
-
-function love.focus(bool)
-
+  
+if box.x <= 0 then
+box.x = 0
+box.direction = 1
 end
-
-function love.keypressed( key, unicode )
-
-end
-
-function love.keyreleased( key, unicode )
-
-end
-
-function love.mousepressed( x, y, button )
-print(x, y, button)
-end
-
-function love.mousereleased( x, y, button )
-end
-
-function love.quit()
 end
